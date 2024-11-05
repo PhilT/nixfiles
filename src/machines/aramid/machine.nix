9d13ffb7f6a3edf8ff6eeeff8d412e341db8eeee
Phil Thompson <34678+PhilT@users.noreply.github.com>
2024-11-05 07:49:33 +0000

    Switch to latest kernel that ZFS supports

--- src/machines/aramid/machine.nix
+++ src/machines/aramid/machine.nix
@@ -1,9 +1,8 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   machine = "aramid";
   username = "phil";
   fullname = "Phil Thompson";
   nixfs.enable = true;
-  vulkan.enable = false;
 
   networking.hostId = "d549b408";
   boot.kernelParams = [ "nohibernate" ];  # Hibernate not supported on ZFS (no swapfiles)
@@ -11,6 +10,17 @@
   services.zfs.autoScrub.enable = true;   # Setup a scrub schedule
   boot.zfs.forceImportRoot = false;       # Hopefully don't need backwards compatibility
   boot.zfs.devNodes = "/dev/disk/by-id";
+
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_6.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+            url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+            sha256 = "sha256-VeW8vGjWZ3b8RolikfCiSES+tXgXNFqFTWXj0FX6Qj4=";
+      };
+      version = "6.10.14";
+      modDirVersion = "6.10.14";
+    };
+  });
 
   fileSystems = {
     "/" = {
