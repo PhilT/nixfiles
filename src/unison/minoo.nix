1c42e76d26fd4aec57937d18f7f340c27f86de1e
Phil Thompson <34678+PhilT@users.noreply.github.com>
2024-11-07 22:17:55 +0000

    Reorg of Unison setup to include Minoo

--- src/unison/minoo.nix
+++ src/unison/minoo.nix
@@ -1,22 +1,28 @@
-# Sync Unison between Phone and Spruce (Desktop) or Aramid (X1C)
-# FIXME: Some duplication exists between phone.nix and spruce.nix
+# Sync Unison with Minoo
+# Runs on Spruce and Aramid
+# FIXME: Some duplication exists between minoo.nix and suuno.nix
 
-{ name }: { config, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
+  name = "minoo";
   paths = [
     "books"
     "documents"
     "music"
+    "music_extra"
     "notes"
-    "DCIM"
-    "pictures/showcase"
+    "other"
+    "pictures"
+    "screenshots"
+    "studio"
     "sync"
+    "thunderbird_profile"
     "txt"
+    "videos"
   ];
   pathsConfig = lib.lists.foldr (path: str: "path = ${path}\n${str}") "";
-  mountsConfig = lib.lists.foldr (path: str: "mountpoint = ${path}\n${str}") "";
-  root = "/data";
+  root = config.dataDir;
   folders = map (path: "d ${root}/${path} - ${config.username} users -") paths;
   extractIpAddress = "sed -En 's/.*${name} \\((.*)\\)/\\1/p'";
 in
@@ -26,11 +32,9 @@ in
   ];
 
   environment.systemPackages = with pkgs; [
-    (writeShellScriptBin name ''
+    (writeShellScriptBin "sync_${name}" ''
       device_ip=`nmap -sn 192.168.1.0/24 | ${extractIpAddress}`
-      sshfs $device_ip:/ /mnt/${name} -p 8022 -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
-      unison ${root} /mnt/${name} -include ${name} $@
-      mount | grep ${name} > /dev/null && umount /mnt/${name}
+      unison ${root} ssh://$device_ip//${root} -include ${name} $@
     '')
   ];
 
@@ -39,20 +43,13 @@ in
     text = ''
       include common
 
-      perms = 0
-      dontchmod = true
-
       ${pathsConfig paths}
-      ${mountsConfig paths}
-      forcepartial = Path pictures/showcase /data
-      forcepartial = Path music /data
     '';
   };
 
   systemd.tmpfiles.rules = [
     "d ${config.userHome} - ${config.username} users -"
     "d ${config.userHome}/.unison - ${config.username} users -"
-    "d /mnt/${name} - ${config.username} users -"
 
     "L+ ${config.userHome}/.unison/${name}.prf - - - - /etc/config/unison/${name}.prf"
   ] ++ folders;
