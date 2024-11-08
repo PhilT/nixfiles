6b6cc79a94457320e74208929107794258408c21
Phil Thompson <34678+PhilT@users.noreply.github.com>
2024-11-08 11:25:51 +0000

    Fix loading and running of 2nd drive functions

---
+++ lib/disks2.sh
@@ -0,0 +1,15 @@
+# Key-based drive encryption functions
+
+generate_key() {
+  local keyfile=$1
+
+  sudo dd if=/dev/urandom bs=32 count=1 of=$keyfile
+  sudo chmod 400 $keyfile
+}
+
+change_key() {
+  local pool=$1
+  local keyfile="file://$1"
+
+  sudo zfs change-key -o keyformat=raw -o keylocation=$keyfile $pool
+}
