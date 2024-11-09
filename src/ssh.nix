35bf36682a3fa408e549a4e01905523831ccc310
Phil Thompson <34678+PhilT@users.noreply.github.com>
2024-11-09 22:10:26 +0000

    WIP: SSH fixes

--- src/ssh.nix
+++ src/ssh.nix
@@ -15,9 +15,12 @@ in
     };
   };
 
+  # Client SSH program checks knownHosts when connecting to a server
+  # This file is on the client
   programs.ssh.knownHosts = {
     "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+    "192.168.1.248".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGR9XFTdYSj2RkPy4OaAVJvzP5D5o9JbUUYzlK2zc/aX";
   };
 
   programs.ssh.extraConfig = ''
@@ -32,7 +35,8 @@ in
       IdentityFile ${homeDir}/.ssh/id_ed25519_github
   '';
 
-  # Used by Unison to authorize connection to each other
+  # Used by Unison to authorize a connection from an incoming client.
+  # This file is on the server.
   # Also copied to phone (should be available in /etc/ssh/authorized_keys)
   users.users."${config.username}".openssh.authorizedKeys.keys = [
     (builtins.readFile ../secrets/id_ed25519_spruce.pub)
