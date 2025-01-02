393661888306677961a82157b309c00c3d436297
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-01-02 19:22:02 +0000

    Theme updates, process-compose, .ignore
    * Ensure Kitty is using the up-to-date theme from Catppuccin
    * Add theme for process-compose
    * Point to correct path for /etc/ignore

--- src/development.nix
+++ src/development.nix
@@ -9,26 +9,46 @@
     direnv.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    docker-compose
-    devbox
-    quickemu
+  environment = {
+    systemPackages = with pkgs; [
+      docker-compose
+      devbox
+      quickemu
 
-    (writeShellScriptBin "qemu-system-x86_64-uefi" ''
-      qemu-system-x86_64 \
-        -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-        "$@"
-    '')
+      (writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
 
 
-    (writeShellScriptBin "matter" ''
-      cd $CODE/matter
-      nix-shell shell.nix --run "nvim -S Session.vim"
-    '')
+      (writeShellScriptBin "matter" ''
+        cd $CODE/matter
+        nix-shell shell.nix --run "nvim -S Session.vim"
+      '')
 
-    (writeShellScriptBin "gox" ''
-      cd $CODE/matter
-      nix-shell shell.nix --run "gox -s 2"
-    '')
+      (writeShellScriptBin "gox" ''
+        cd $CODE/matter
+        nix-shell shell.nix --run "gox -s 2"
+      '')
+    ];
+
+    etc = {
+      "process-compose/theme.yaml".source = ../dotfiles/process-compose-theme.yaml;
+
+      # TODO: disable_exit_confirmation does not work yet as I think devbox is using an
+      # older version of process-compose. Hopefully this will be updated soon
+      "process-compose/settings.yaml".text = ''
+        theme: Custom Style
+        sort:
+          by: NAME
+          isReversed: false
+        disable_exit_confirmation: true
+      '';
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "L+ ${config.xdgConfigHome}/process-compose - - - - /etc/process-compose"
   ];
 }
