d4a43f3a280b65448b725e9f2702dd05acb6e686
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-01-04 20:07:28 +0000

    Add some logging to Unison service

--- src/unison/suuno.nix
+++ src/unison/suuno.nix
@@ -36,9 +36,13 @@ in
     sshfs
 
     (writeShellScriptBin "sync_${config.unison.target}" ''
+      echo "*** ${config.unison.target} *** Mounting"
       ${pkgs.sshfs}/bin/sshfs ${config.unison.target}:/ /mnt/${config.unison.target} -p 2222 -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3
+      echo "*** ${config.unison.target} *** Running Unison"
       ${pkgs.unison}/bin/unison ${config.dataDir} /mnt/${config.unison.target} -include common $@
+      echo "*** ${config.unison.target} *** Unmounting"
       /run/wrappers/bin/mount | grep ${config.unison.target} > /dev/null && /run/wrappers/bin/umount /mnt/${config.unison.target}
+      echo "*** ${config.unison.target} *** Done"
     '')
   ];
 
