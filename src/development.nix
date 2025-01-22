639427813004547a57beeb6049693c875e52d4fa
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-01-22 09:38:36 +0000

    Separate out devbox setup

--- src/development.nix
+++ src/development.nix
@@ -1,18 +1,13 @@
 { config, lib, pkgs, ... }: {
   imports = [
+    ./devbox.nix
   ] ++ lib.optional (builtins.pathExists ./work.nix) ./work.nix; # Copy from work.nix.example to use
 
   virtualisation.docker.enable = true;
 
-  programs = {
-    # Autorun nix-shell when entering a dir with a shell.nix (e.g. a Ruby or .NET project)
-    direnv.enable = true;
-  };
-
   environment = {
     systemPackages = with pkgs; [
-      docker-compose
-      devbox
+      docker-compose # TODO: Should be able to remove once process-compose is rolled out
       #quickemu # FIXME: Broken package
 
       (writeShellScriptBin "qemu-system-x86_64-uefi" ''
@@ -33,22 +28,5 @@
       '')
     ];
 
-    etc = {
-      "process-compose/theme.yaml".source = ../dotfiles/process-compose-theme.yaml;
-
-      # TODO: Most of these settings do not work yet as I think devbox is using an
-      # older version of process-compose. Hopefully this will be updated soon
-      "process-compose/settings.yaml".text = ''
-        theme: Custom Style
-        sort:
-          by: STATUS,NAME
-          isReversed: false
-        disable_exit_confirmation: true
-      '';
-    };
   };
-
-  systemd.tmpfiles.rules = [
-    "L+ ${config.xdgConfigHome}/process-compose - - - - /etc/process-compose"
-  ];
 }
