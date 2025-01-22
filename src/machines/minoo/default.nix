9cca98968b8a423bf560cccf870095892589463f
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-01-22 09:38:36 +0000

    Separate out devbox setup

--- src/machines/minoo/default.nix
+++ src/machines/minoo/default.nix
@@ -23,12 +23,17 @@
     # Desktop
     ../../desktop/default.nix
     ../../desktop/light.nix
+
+    # Running webservers
+    ../../devbox.nix
   ];
 
   # How do we supply the key?
   # Need to change to keyfile instead of prompt?
   # Maybe not though, if permanently on and connected to the TV that
   # might enough to keep simple password login.
+  # FIXME: Would be handy if I need to reboot it though to supply the password file
+  # via SSH.
 
   hardware.graphics.enable = true; # TODO: Move into generic config
 }
