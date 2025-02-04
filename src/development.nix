8ca201311a4ec8a33caadca33743960f3623a36e
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-02-04 19:19:16 +0000

    Move qemu script command, reformat comment

--- src/development.nix
+++ src/development.nix
@@ -1,9 +1,9 @@
+# work.nix is from work.nix.example and isn't versioned.
+# This one is synced with Unison.
+
 { config, lib, pkgs, ... }: {
   imports = [
     ./devbox.nix
-                                                              # Copy from work.nix.example
-                                                              # This one is synced with Unison
-                                                              # and ignored in VC.
   ] ++ lib.optional (builtins.pathExists /data/work/work.nix) /data/work/work.nix;
 
   virtualisation.docker.enable = true;
@@ -11,13 +11,6 @@
   environment = {
     systemPackages = with pkgs; [
       docker-compose # TODO: Should be able to remove once process-compose is rolled out
-
-      (writeShellScriptBin "qemu-system-x86_64-uefi" ''
-        qemu-system-x86_64 \
-          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-          "$@"
-      '')
-
 
       (writeShellScriptBin "matter" ''
         cd $CODE/matter
--- src/qemu.nix
+++ src/qemu.nix
@@ -5,6 +5,13 @@
     systemPackages = [
       samba
       qemu
+
+      (writeShellScriptBin "qemu-system-x86_64-uefi" ''
+        qemu-system-x86_64 \
+          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+          "$@"
+      '')
+
     ];
   };
 }
