e441602361a1aebdaaefd86f9f90c3bc16c63f64
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-02-05 09:12:07 +0000

    wip

--- src/machines/spruce/machine.nix
+++ src/machines/spruce/machine.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  vendor = "10de"; # Duplicated in gpu_ids.ini
+  gpu = "2684";
+  audio = "22ba";
+in {
   networking.hostId = "79ef3090";
   machine = "spruce";
   username = "phil";
@@ -15,6 +20,42 @@
     options = [ "rw" "uid=1000" ];
   };
 
-  boot.initrd.kernelModules = [ "nouveau" ]; # Native resolution in early KMS (Kernel Mode Setting)
-  boot.kernelParams = [ "nouveau.runpm=0" "intel_iommu=on" ];
+  boot.initrd.kernelModules = [
+    #"nouveau" # Native resolution in early KMS (Kernel Mode Setting)
+  ];
+  boot.kernelParams = [
+    #"nouveau.runpm=0"
+    #"nvidia-drm.modeset=0" # Probably not needed as I'm not loading the propriatary Nvidia drivers
+
+    "intel_iommu=on"
+    "iommu=pt" # Might be needed for performance. Test and see
+  ];
+
+  # TODO: I don't think this is needed
+#  boot.blacklistedKernelModules = [
+#    "nouveau"
+#  ];
+
+  boot.extraModprobeConfig = ''
+    options vfio-pci ids=${vendor}:${gpu},${vendor}:${audio}
+  '';
+
+  boot.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" "kvm-intel" ];
+
+  services.udev.extraRules = ''
+    KERNEL=="vfio", MODE="0660", GROUP="vfio"
+    KERNEL=="vfio/*", MODE="0660", GROUP="vfio"
+  '';
+
+  # FIXME: This didn't seem to work - as in, when ignoring the LUKS password prompt on
+  # the machine and trying to SSH in remotely, I got host not found.
+  # Be good to test again once I've switched to ZFS
+  boot.initrd.network = {
+    enable = true;
+    ssh.enable = true;
+    ssh.hostKeys = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    ssh.authorizedKeys = [
+      (builtins.readFile ../../../secrets/id_ed25519_spruce.pub)
+    ];
+  };
 }
--- src/qemu.nix
+++ src/qemu.nix
@@ -1,17 +1,23 @@
+# https://astrid.tech/2022/09/22/0/nixos-gpu-vfio/
+
+
 { config, lib, pkgs, ... }:
 {
   virtualisation.spiceUSBRedirection.enable = true;
-  environment = with pkgs; {
-    systemPackages = [
-      samba
-      qemu
+  environment.systemPackages = with pkgs;[
+    samba
+    (qemu.overrideAttrs (oldAttrs: { secureBoot = true; msVarsTemplate = true; }))
+    swtpm
+    cpuset
 
-      (writeShellScriptBin "qemu-system-x86_64-uefi" ''
-        qemu-system-x86_64 \
-          -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
-          "$@"
-      '')
+    (writeShellScriptBin "qemu-system-x86_64-uefi" ''
+      qemu-system-x86_64 \
+        -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+        "$@"
+    '')
+  ];
 
-    ];
-  };
+  # FIXME: Or remove: Calling qemu as root anyway so this is probably not needed
+  users.groups.vfio = {};
+  users.users."${config.username}".extraGroups = [ "vfio" "kvm" ];
 }
