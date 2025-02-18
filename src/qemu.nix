4facb4985a9d59476d92fb88015af326a0acfd0b
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-02-18 20:52:21 +0000

    Add support for LookingGlass

--- src/qemu.nix
+++ src/qemu.nix
@@ -2,12 +2,23 @@
 
 { config, lib, pkgs, ... }: {
   environment.etc."qemu/bridge.conf".text = "allow br0\n";
+  environment.etc."looking-glass-client.ini".text = ''
+    [win]
+    fullScreen=yes
+
+    [spice]
+    enable=no
+
+    [app]
+    capture=nvfbc
+  '';
   environment.sessionVariables.OVMF_DIR = "${pkgs.OVMF.fd}/FV";
   environment.systemPackages = with pkgs;[
     (OVMF.overrideAttrs (oldAttrs: { secureBoot = true; msVarsTemplate = true; }))
     qemu
     swtpm
-    libhugetlbfs                   # Hugepages
+    libhugetlbfs              # Hugepages
+    looking-glass-client      # View the VM without the monitors being attached
 
     (writeShellScriptBin "qemu-system-x86_64-uefi" ''
       qemu-system-x86_64 \
