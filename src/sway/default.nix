fa9ce7eb6c9d9a44e503774d5e5f7ee25c150efc
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-02-18 19:50:42 +0000

    Switch to iGPU

--- src/sway/default.nix
+++ src/sway/default.nix
@@ -17,7 +17,7 @@ in with colors; {
     config.common.default = "wlr";
     wlr.enable = true;
     wlr.settings.screencast = {
-      output_name = "eDP-1";
+      output_name = "DP-1";
       chooser_type = "simple";
       chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
     };
