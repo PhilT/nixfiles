04561af4f6a9e243b385d3858df8a71716729e48
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-02-20 10:33:27 +0000

    Enable Looking Glass
    * Convert qemu script to Fish
    * Simplified VFIO binding

--- src/qemu.nix
+++ src/qemu.nix
@@ -1,16 +1,10 @@
 # https://astrid.tech/2022/09/22/0/nixos-gpu-vfio/
 
 { config, lib, pkgs, ... }: {
-  environment.etc."qemu/bridge.conf".text = "allow br0\n";
+  #environment.etc."qemu/bridge.conf".text = "allow br0\n";
   environment.etc."looking-glass-client.ini".text = ''
     [win]
     fullScreen=yes
-
-    [spice]
-    enable=no
-
-    [app]
-    capture=nvfbc
   '';
   environment.sessionVariables.OVMF_DIR = "${pkgs.OVMF.fd}/FV";
   environment.systemPackages = with pkgs;[
@@ -27,19 +21,27 @@
     '')
   ];
 
-  # FIXME: Or remove: Calling qemu as root anyway so this is probably not needed
-  users.groups.vfio = {};
-  users.users."${config.username}".extraGroups = [ "vfio" "kvm" ];
-  networking = {
-    # Disable DHCP on physical interface as it's done on the bridge instead
-    useDHCP = false;
-    defaultGateway = "192.168.1.1";
-    nameservers = [ "192.168.1.1" "8.8.8.8" ];
-    bridges."br0".interfaces = [ "eth0" ];
+  systemd.extraConfig = ''
+    DefaultLimitMEMLOCK=infinity
+  '';
 
-    interfaces."br0".ipv4.addresses = [{
-      address = "192.168.1.226";
-      prefixLength = 24;
-    }];
-  };
+  systemd.tmpfiles.rules = [
+    "f /dev/shm/looking-glass 0660 phil users -"
+  ];
+
+  # FIXME: Or remove: Calling qemu as root anyway so this is probably not needed
+  users.groups.qemu = {};
+  users.users."${config.username}".extraGroups = [ "qemu" "kvm" ];
+  #networking = {
+  #  # Disable DHCP on physical interface as it's done on the bridge instead
+  #  useDHCP = false;
+  #  defaultGateway = "192.168.1.1";
+  #  nameservers = [ "192.168.1.1" "8.8.8.8" ];
+  #  bridges."br0".interfaces = [ "eth0" ];
+
+  #  interfaces."br0".ipv4.addresses = [{
+  #    address = "192.168.1.226";
+  #    prefixLength = 24;
+  #  }];
+  #};
 }
