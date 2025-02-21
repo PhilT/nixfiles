cf74fc0e4e48dd4ddeb94fdeea053bb61cc9ef8a
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-02-21 21:27:02 +0000

    Qemu Windows Guest working without sudo, starting with single shortcut

--- src/machines/spruce/machine.nix
+++ src/machines/spruce/machine.nix
@@ -1,13 +1,4 @@
-{ config, pkgs, lib, ... }:
-let
-  vendor = "10de"; # Duplicated in gpu_ids.ini
-  gpu = "2684";
-  audio = "22ba";
-  usbVendor = "";
-  usbController = "";
-  gpuIds = "${vendor}:${gpu},${vendor}:${audio}";
-  usbControllerIds = ""; # ",${usbVendor}:${usbController}"
-in {
+{ config, pkgs, lib, ... }: {
   networking.hostId = "79ef3090";
   machine = "spruce";
   username = "phil";
@@ -27,24 +18,6 @@ in {
   # Ensure both displays are initialized so Plymouth can be displayed on DP-1
   boot.initrd.kernelModules = [ "i915" ];
 
-  boot.kernelParams = [
-    "intel_iommu=on"
-    #"iommu=pt" # Might be needed for performance. Test and see
-    #"hugepagesz=1G"
-    #"hugepages=24"
-  ];
-
-  boot.extraModprobeConfig = ''
-    options vfio-pci ids=${gpuIds}${usbControllerIds}
-  '';
-
-  boot.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" "kvm-intel" ];
-
-  services.udev.extraRules = ''
-    KERNEL=="vfio", MODE="0660", GROUP="vfio"
-    KERNEL=="vfio/*", MODE="0660", GROUP="vfio"
-  '';
-
   # FIXME: This didn't seem to work - as in, when ignoring the LUKS password prompt on
   # the machine and trying to SSH in remotely, I got host not found.
   # Be good to test again once I've switched to ZFS
@@ -56,15 +29,4 @@ in {
       (builtins.readFile ../../../secrets/id_ed25519_spruce.pub)
     ];
   };
-
-  # NEW OPTIONS
-  #boot.kernel.sysctl = {
-  #  "vm.min_free_kbytes" = "57671680";
-  #  "kernel.shmmax" = "25769803776";
-  #};
-
-  # TODO: I don't think this is needed
-  #boot.blacklistedKernelModules = [
-  #  "nouveau"
-  #];
 }
