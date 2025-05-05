baf093e96899a656cf3814f7279f959d83f74016
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-05-05 18:06:32 +0100

    Add multi-OS support to Qemu scripts, bunch of tidy-up

--- src/machines/seedling/machine.nix
+++ src/machines/seedling/machine.nix
@@ -2,10 +2,13 @@
   machine = "seedling";
   username = "phil";
   fullname = "Phil Thompson";
-  vulkan.enable = false;
 
   networking.hostId = "c3a88d59";
-  boot.kernelParams = [ "nohibernate" ];  # Hibernate not supported on ZFS (no swapfiles)
+
+  # Hibernate not supported on ZFS (no swapfiles)
+  # but this is a VM so we can create snapshots with Qemu
+  boot.kernelParams = [ "nohibernate" ];
+
   boot.supportedFilesystems = [ "zfs" ];
   services.zfs.autoScrub.enable = true;   # Setup a scrub schedule
   boot.zfs.forceImportRoot = false;       # Hopefully don't need backwards compatibility
