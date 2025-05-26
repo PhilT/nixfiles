80c2325aad6c4da78e1458e2253b616178e13aa8
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-05-26 11:36:22 +0100

    Fix knownHosts for Hetzner and add some notes

--- src/ssh.nix
+++ src/ssh.nix
@@ -1,6 +1,8 @@
+# I considered linking .ssh to the persistedHomeDir but actually this would result in knownHosts being updated should I connect
+# to a new server which I don't want to persist between reboots.
 { config, lib, pkgs, ... }:
 let
-  FABRIK_HETZNER_SERVER_IP = "95.216.193.4";
+  HETZNER_SERVER_IP = "95.216.193.4";
 in {
   services.fail2ban.enable = true;
 
@@ -33,17 +35,21 @@ in {
   };
 
   # Client SSH program checks knownHosts when connecting to a server
-  # This file is on the client
+  # This file is on the client at /etc/ssh/ssh_known_hosts
+  # Hetzner is a root login e.g. ssh root@${HETZNER_SERVER_IP}
+  # When looking up github.com /etc/ssh/ssh_known_hosts is checked
+  # These keys can be retreived by doing e.g. `ssh-keyscan -t ed25519 github.com` or `ssh-keyscan -t ecdsa minoo`
   programs.ssh.knownHosts = {
-    "${FABRIK_HETZNER_SERVER_IP}".publicKey = (builtins.readFile ../secrets/id_ed25519_spruce_hetzner.pub);
+    "${HETZNER_SERVER_IP}".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINo+WfA+c5sqnu/jPJN021gpfiVRRhxaYxKq7Kti9H7X";
     "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
-    "minoo".publicKey = (builtins.readFile ../secrets/id_ecdsa_minoo.pub);
+    "minoo".publicKey = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOWkHHuzy/5g47M7vI1FPL6lnZPGKJF1sd6m39y19Skp2gIPnlcyLt8671QgVDeXWisB78Bgm75XHatm0r5ECqc=";
     "suuno".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGGiHFo6GiqCA3YKp58oP7RELGJ362G0aJyR0NgViu5";
   };
 
+  # These options are applied while connecting to GitHub
   programs.ssh.extraConfig = ''
-    Host ${FABRIK_HETZNER_SERVER_IP}
+    Host ${HETZNER_SERVER_IP}
       IdentitiesOnly yes
       PreferredAuthentications publickey
       IdentityFile ${config.persistedHomeDir}/ssh/id_ed25519_hetzner
