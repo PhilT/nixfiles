2a9c24a54aca9ca8c3fcea87e3aaa7bce994529d
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-06-30 09:15:28 +0100

    Try SunVox, add notes on bluetooth
    * Disable VST plugins and tryout SunVox
    * Add notes on how to setup Bluetooth on Windows and Linux

--- src/studio.nix
+++ src/studio.nix
@@ -1,26 +1,26 @@
 { pkgs, ... }: {
   environment = {
-    sessionVariables = {
-      VST_PATH = "/run/current-system/sw/lib/vst/";
-      VST3_PATH = "/run/current-system/sw/lib/vst3/";
-    };
+    # sessionVariables = {
+    #   VST_PATH = "/run/current-system/sw/lib/vst/";
+    #   VST3_PATH = "/run/current-system/sw/lib/vst3/";
+    # };
 
     systemPackages = with pkgs; [
-      # TODO: MOVE TO STUDIO?
       renoise
-      surge-XT
-      stochas
-      librearp
-      lsp-plugins
-      odin2
-      cardinal
-      fire
+      sunvox
+      # surge-XT
+      # stochas
+      # librearp
+      # lsp-plugins
+      # odin2
+      # cardinal
+      # fire
 
       # VST plugins
-      (callPackage ./vst/sala.nix {})
+      # (callPackage ./vst/sala.nix {})
       # (callPackage ./vst/ot_piano_s.nix {})
-      (callPackage ./vst/argotlunar.nix {})
-      (callPackage ./vst/excite_snare_drum.nix {})
+      # (callPackage ./vst/argotlunar.nix {})
+      # (callPackage ./vst/excite_snare_drum.nix {})
     ];
   };
 }
