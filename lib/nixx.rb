6b4f23a4a506d61691c3fd83ef1e202814f7020b
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-08-09 22:18:01 +0100

    THOR: Add clean, channels, sha, sudo, run

--- lib/nixx.rb
+++ lib/nixx.rb
@@ -12,6 +12,8 @@ class Nixx < Thor
     desc: "Switch to the new config on next boot"
   option :upgrade, type: :boolean, default: false, aliases: :u,
     desc: "Upgrade the channel and switch"
+  option :clean, type: :boolean, default: false,
+    desc: "Run nix-collect-garbage -d"
   option :machine, type: :string, default: nil, aliases: :m,
     desc: "Build a different machine (aramid/minoo/seedling/spruce)"
   option :module, type: :string, default: nil, aliases: :o,
@@ -20,23 +22,59 @@ class Nixx < Thor
     desc: "Show trace"
   def build
     @machine = options[:machine] || `hostname`.strip
-    @sudo = `whoami`.strip == "root" ? "" : "sudo " # When run from nixos-enter we don't need sudo
+    @upgrade = true if options[:upgrade]
 
     command = "build"
-    command = "switch" if options[:switch]
+    command = "switch" if options.slice(:switch, :upgrade, :clean).any?
     command = "boot" if options[:boot]
-    command = "upgrade" if options[:upgrade]
     modul = options[:module] || "default.nix"
     configuration_nix = File.join(ROOT_DIR, "src/machines/#{@machine}/#{modul}")
     etc_dir = ephemeral_os? ? "/data/etc" : "/etc"
 
-    system("#{@sudo} NIXOS_CONFIG=#{configuration_nix} nixos-rebuild #{command}#{trace} |& nom")
+    add_channels
+
+    puts "[#{command.upcase}] #{@machine}"
+    sudo("nix-collect-garbage -d") if options[:clean]
+    sudo("nix-channel --update") if @upgrade
+    sudo("NIXOS_CONFIG=#{configuration_nix} nixos-rebuild #{command}#{trace} |& nom")
+  end
+
+  desc "sha URL", "Fetch a SHA256 for the given package"
+  def sha(url)
+    # Might need to add --unpack if we're prefetching an archive
+    run("nix-prefetch-url #{url}")
   end
 
   desc "credentials", "Manage encrypted credentials"
   subcommand "credentials", Credentials
 
   private
+
+  def add_channels
+    print "[CHANNELS] "
+    channel_list = sudo("nix-channel --list", return_output: true).strip.gsub("\n", " ")
+    if channel_list =~ /catppuccin.*nixos-hardware/
+      puts "Up-to-date"
+    else
+      puts "Updating"
+      sudo("nix-channel --add https://github.com/catppuccin/nix/archive/main.tar.gz catppuccin")
+      sudo("nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware")
+      @upgrade = true
+    end
+  end
+
+  def sudo(cmd, return_output: false)
+    @sudo ||= `whoami`.strip == "root" ? "" : "sudo " # When run from nixos-enter we don't need sudo
+    run("#{@sudo}#{cmd}", return_output:)
+  end
+
+  def run(cmd, return_output: false)
+    if return_output
+      `#{cmd}`
+    else
+      system(cmd)
+    end
+  end
 
   def trace
     options[:trace] ? " --show-trace" : ""
@@ -45,5 +83,4 @@ class Nixx < Thor
   def ephemeral_os?
     %w[aramid seedling].include?(@machine)
   end
-
 end
