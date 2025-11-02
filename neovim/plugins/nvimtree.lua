7d11e2cefbac66a194d331bc72de9832d1153fc6
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-11-02 19:40:57 +0000

    Map e to up for Colemak in nvimtree

--- neovim/plugins/nvimtree.lua
+++ neovim/plugins/nvimtree.lua
@@ -12,5 +12,20 @@ require("nvim-tree").setup {
   },
   live_filter = {
     always_show_folders = false
-  }
+  },
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+
+    -- Default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- Custom mappings for Colemak navigation
+    local function opts(desc)
+      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- Remove default 'e' (rename) and remap to up (mirrors n for down)
+    vim.keymap.del('n', 'e', { buffer = bufnr })
+    vim.keymap.set('n', 'e', 'k', opts('Up'))
+  end
 }
