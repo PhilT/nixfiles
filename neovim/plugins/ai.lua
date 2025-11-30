950fe9954a2fbcbc933da3f06fa739f507f92424
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-11-30 04:43:41 +0000

    Tweak Supermaven keybinds

--- neovim/plugins/ai.lua
+++ neovim/plugins/ai.lua
@@ -1,5 +1,12 @@
 require("supermaven-nvim").setup({
   keymaps = {
     accept_suggestion = "<S-Tab>",
+    clear_suggestion = "<C-]>",
+    accept_word = "<C-l>",
   }
 })
+
+-- Add Colemak equivalent for accept_word
+vim.keymap.set('i', '<C-i>', function()
+  require('supermaven-nvim.completion_preview').on_accept_suggestion_word()
+end, { desc = 'Supermaven accept word (Colemak)' })
