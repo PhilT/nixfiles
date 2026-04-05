f22d9395d3242d09fc4ad75300c20ba9fe8620e4
Phil Thompson <34678+PhilT@users.noreply.github.com>
2026-04-05 09:49:06 +0100

    Fix Thunar file open: create kitty-nvim desktop entry for mime types

--- src/mimetypes.nix
+++ src/mimetypes.nix
@@ -1,8 +1,18 @@
 { config, lib, pkgs, ... }:
-{
+let
+  kittyNvim = pkgs.makeDesktopItem {
+    name = "kitty-nvim";
+    desktopName = "Neovim";
+    exec = "kitty nvim %F";
+    type = "Application";
+    mimeTypes = [ "text/plain" "text/markdown" ];
+  };
+in {
+  environment.systemPackages = [ kittyNvim ];
+
   xdg.mime.defaultApplications = {
-    "text/plain" = "kitty nvim";
-    "text/markdown" = "kitty nvim";
+    "text/plain" = "kitty-nvim.desktop";
+    "text/markdown" = "kitty-nvim.desktop";
     "x-scheme-handler/http" = "chromium-browser.desktop";
     "x-scheme-handler/https" = "chromium-browser.desktop";
     "text/html" = "chromium-browser.desktop";
