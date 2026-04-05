ad11782f1753d02e590d10410d7aff00390e9835
Phil Thompson <34678+PhilT@users.noreply.github.com>
2026-04-05 10:07:11 +0100

    Open URLs in new tabs in Chromium

--- src/mimetypes.nix
+++ src/mimetypes.nix
@@ -7,14 +7,22 @@ let
     type = "Application";
     mimeTypes = [ "text/plain" "text/markdown" ];
   };
+
+  chromiumTab = pkgs.makeDesktopItem {
+    name = "chromium-tab";
+    desktopName = "Chromium";
+    exec = "chromium --new-tab %U";
+    type = "Application";
+    mimeTypes = [ "x-scheme-handler/http" "x-scheme-handler/https" "text/html" ];
+  };
 in {
-  environment.systemPackages = [ kittyNvim ];
+  environment.systemPackages = [ kittyNvim chromiumTab ];
 
   xdg.mime.defaultApplications = {
     "text/plain" = "kitty-nvim.desktop";
     "text/markdown" = "kitty-nvim.desktop";
-    "x-scheme-handler/http" = "chromium-browser.desktop";
-    "x-scheme-handler/https" = "chromium-browser.desktop";
-    "text/html" = "chromium-browser.desktop";
+    "x-scheme-handler/http" = "chromium-tab.desktop";
+    "x-scheme-handler/https" = "chromium-tab.desktop";
+    "text/html" = "chromium-tab.desktop";
   };
 }
