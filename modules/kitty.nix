ff3bb77aa93d43cd3966b5ee5bd08de1fc686d79
Phil Thompson <34678+PhilT@users.noreply.github.com>
2026-05-29 18:46:49 +0100

    kitty: show hyperlink URL on hover

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -49,6 +49,7 @@ in {
           scrollback_lines -1
           scrollback_pager_history_size 0
           scrollback_pager /run/current-system/sw/bin/less -R
+          show_hyperlink_targets yes
 
           # Support Shift+Enter in Claude Code
           map --when-focus-on title:claude shift+enter send_text normal,application \\\n
